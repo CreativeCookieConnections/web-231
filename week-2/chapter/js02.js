0d68db13ef30f0ac64e12510031f0721568fe236
CreativeCookieConnections <creativecookieconnections@gmail.com>
2025-08-21 07:47:08 -0500

    Worked on Week 2 html and js files

---
+++ week-2/chapter/js02.js
@@ -0,0 +1,21 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Chapter case
+
+      Fan Trick Fine Art Photography
+      Variables and functions
+      Author: Aisha Keller
+      Date:   08/21/2025
+
+      Filename: js02.js
+ */
+
+// set the form's default values
+function setupForm() {
+  document.getElementById("photoNum") .value = 1;
+  document.getElementById("photoHrs") .value = 2;
+  document.getElementById("makeBook") .checked = false;
+  document.getElementById("photoRights") .checked = false;
+  document.getElementById("photoDist") .value = 0;
+}
+
